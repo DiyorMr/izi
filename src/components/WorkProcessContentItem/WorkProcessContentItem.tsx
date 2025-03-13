327531bf0e8d0b47e94db3d708d29d05ca695d24
boburastonov <boburastonov33@gmail.com>
2025-03-13 17:15:49 +0500

    Add work process section without divider scroll animation

---
+++ src/components/WorkProcessContentItem/WorkProcessContentItem.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./WorkProcessContentItem.scss";
+import Button from "../Button";
+
+interface WorkProcessContentItemProps {
+  title: string;
+  text: string;
+  path: string;
+  buttonText: string;
+  img: string;
+}
+
+const WorkProcessContentItem: React.FC<WorkProcessContentItemProps> = ({
+  title,
+  text,
+  path,
+  buttonText,
+  img,
+}) => {
+  return (
+    <div className="work-process_content-item">
+      <div className="item-content">
+        <h2 className="item-content_tile">{title}</h2>
+        <p className="item-content_text">{text}</p>
+        <Button path={path} text={buttonText} />
+      </div>
+      <div className="item-img">
+        <img src={img} alt={title} />
+      </div>
+    </div>
+  );
+};
+
+export default WorkProcessContentItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import "./Home.scss";
 import Button from "@/components/Button";
 import Header from "@/components/Header";
 import SlideItem from "@/components/SliderItem";
+import WorkProcessContentItem from "@/components/WorkProcessContentItem";
 
 const Home = () => {
   return (
@@ -36,6 +37,47 @@ const Home = () => {
             <SlideItem image="/slide-3.png" />
             <SlideItem image="/slide-4.png" />
             <SlideItem image="/slide-5.png" />
+          </div>
+        </section>
+
+        {/* Work Process section */}
+        <section className="work-process" id="work-process">
+          <div className="container work-process_container">
+            <h2 className="work-process_title">
+              How it
+              <span className="work-process_subtitle"> works?</span>
+            </h2>
+            <div className="work-process_content">
+              <WorkProcessContentItem
+                title="Language chousing"
+                text="LoremLoremLoremLoremLoremLoremLoremLoremLorem 
+                LoremLoremLoremLoremLoremLoremLoremLoremLoremLorem"
+                path="#"
+                buttonText="Chouse Language"
+                img="/process-img.png"
+              />
+              <WorkProcessContentItem
+                title="Uploading audio or video file"
+                text="LoremLoremLoremLoremLoremLoremLoremLoremLoremLorem LoremLoremLoremLoremLoremLoremLoremL"
+                path="#"
+                buttonText="Upload file"
+                img="/process-img.png"
+              />
+              <WorkProcessContentItem
+                title="Find out what to do"
+                text="LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem"
+                path="#"
+                buttonText="Give command"
+                img="/process-img.png"
+              />
+              <WorkProcessContentItem
+                title="Download your file"
+                text="LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLorem"
+                path="#"
+                buttonText="Download file"
+                img="/process-img.png"
+              />
+            </div>
           </div>
         </section>
       </main>
