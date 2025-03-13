63a6c3c6a30f2fd3880a18d20a55ed725a1b1e70
boburastonov <boburastonov33@gmail.com>
2025-03-13 10:47:34 +0500

    Add configuration to main file and install necessary packages

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "@/assets/styles/main.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
