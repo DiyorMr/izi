a8532c64c70df762de08ccf0bf2a89a881dad009
boburastonov <boburastonov33@gmail.com>
2025-03-13 12:04:13 +0500

    Add hero section and it's styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
+import "./Home.scss";
+import Button from "@/components/Button";
 import Header from "@/components/Header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main>
+        {/* Hero section */}
+        <section className="hero">
+          <div className="container hero_container">
+            <div className="hero_info">
+              <h1 className="hero_title">
+                Your AI-Powered
+                <span className="hero_subtitle"> Design</span> Assistant
+              </h1>
+              <p className="hero_text">
+                LoremLoremLoremLoremLoremLorem LoremLoremLoremLorem
+                LoremLoremLoremLoremLoremLoremLoremLoremLorem
+              </p>
+              <Button path="#contact" text="Start now" />
+            </div>
+            <div className="hero_img">
+              <img src="/hero-img.png" alt="" />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
