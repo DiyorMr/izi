47535a02e89fc35b0a0f9f95f6c38e54a5634ee0
boburastonov <boburastonov33@gmail.com>
2025-03-13 10:48:28 +0500

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+import "./Header.scss";
+import Button from "../Button";
+
+const Headerr = () => {
+  return (
+    <header className="header">
+      <div className="container header_container">
+        <Link className="logo-wrapper" to="/">
+          <img className="logo" src="/logo.svg" alt="Logo" />
+        </Link>
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a className="nav_link" href="#about-project">
+                About project
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav_link" href="#work-process">
+                How it works
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav_link" href="#feedbacks">
+                Feedbacks
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <Button path="#contact" text="Get started" />
+      </div>
+    </header>
+  );
+};
+
+export default Headerr;
