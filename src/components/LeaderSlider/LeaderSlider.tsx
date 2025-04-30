1ae07d2d581b12ff88ec4c3989ac5f9d62b10bce
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2025-04-30 17:41:55 +0500

    add change

--- src/components/LeaderSlider/LeaderSlider.tsx
+++ src/components/LeaderSlider/LeaderSlider.tsx
@@ -6,37 +6,53 @@ import "./LeaderSlider.scss";
 
 // import required modules
 import { Autoplay } from "swiper/modules";
-import SlideItem from "../SliderItem";
+import { LeaderItem } from "../LeaderItem";
+
 
 const LeaderSlider = () => {
   return (
     <div className="leader-box">
-        <h2  className="leader-title">We work with the industry leaders</h2>
-        <Swiper
-      slidesPerView={4}
-      loop={true}
-      speed={5000}
-      autoplay={{
-        delay: 0,
-        disableOnInteraction: false,
-      }}
-      allowTouchMove={false}
-      modules={[Autoplay]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <SlideItem image="/images/leader-one.jpg" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SlideItem image="/images/leader-two.jpg" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SlideItem image="/images/leader-three.jpg" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SlideItem image="/images/leader-four.jpg" />
-      </SwiperSlide> 
-    </Swiper>
+      <h2 className="leader-title">We work with the industry leaders</h2>
+      <Swiper
+        slidesPerView={4}
+        loop={true}
+        speed={5000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+        }}
+        allowTouchMove={false}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-one.jpg" title="WE DIGITAL" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-eight.png" title="ALPHA" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-three.jpg" title="OSNOVA" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-four.jpg" title="AIMLAB" />
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-one.jpg" title="WE DIGITAL" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-two.jpg" title="ALPHA" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-three.jpg" title="OSNOVA" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LeaderItem image="/images/leader-four.jpg" title="AIMLAB" />
+        </SwiperSlide>
+
+
+      </Swiper>
     </div>
   );
 };
