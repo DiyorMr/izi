9f7e6e851e6e36541ea61963c9822d9b8cf09350
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2025-05-10 00:15:55 +0500

    About  page change completed

--- src/components/LeaderSlider/LeaderSlider.tsx
+++ src/components/LeaderSlider/LeaderSlider.tsx
@@ -1,61 +1,45 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "./LeaderSlider.scss";
-
-// import required modules
 import { Autoplay } from "swiper/modules";
 import { LeaderItem } from "../LeaderItem";
 
+const slides = [
+  { image: "/images/leader-one.jpg", title: "WE DIGITAL" },
+  { image: "/images/leader-eight.png", title: "ALPHA" },
+  { image: "/images/leader-three.jpg", title: "OSNOVA" },
+  { image: "/images/leader-four.jpg", title: "AIMLAB" },
+  { image: "/images/leader-two.jpg", title: "ALPHA" },
+];
+
+// 12 tagacha slayd bo'lishi uchun ko'paytirib oldik
+const repeatedSlides = [...slides, ...slides, ...slides.slice(0, 2)];
 
 const LeaderSlider = () => {
   return (
     <div className="leader-box">
       <h2 className="leader-title">We work with the industry leaders</h2>
-    <div className="box-leader">
-    <Swiper
-        slidesPerView={5}
-        loop={true}
-        speed={5000}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-          
-        }}
-        loopAdditionalSlides={5}
-        allowTouchMove={false}
-        modules={[Autoplay]}
-      >
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-one.jpg" title="WE DIGITAL" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-eight.png" title="ALPHA" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-three.jpg" title="OSNOVA" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-four.jpg" title="AIMLAB" />
-        </SwiperSlide>
-        
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-one.jpg" title="WE DIGITAL" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-two.jpg" title="ALPHA" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-three.jpg" title="OSNOVA" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <LeaderItem image="/images/leader-four.jpg" title="AIMLAB" />
-        </SwiperSlide>
-
-
-      </Swiper>
-    </div>
+      <div className="box-leader">
+        <Swiper
+          className="mySwiper" // className qo'shildi
+          slidesPerView={5}
+          loop={true}
+          speed={3000} // Tezlikni 3000 ga tushirish
+          autoplay={{
+            delay: 1, // delay 1 qilib o'zgartirildi
+            disableOnInteraction: false,
+          }}
+          loopAdditionalSlides={10} // 10 ta qo'shimcha slayd
+          allowTouchMove={false}
+          modules={[Autoplay]}
+        >
+          {repeatedSlides.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <LeaderItem image={item.image} title={item.title} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
